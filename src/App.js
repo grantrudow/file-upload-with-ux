e7f9eac1f6e0f1bd70f0c307e187edea8a354e3a
Grant Rudow <webdev.grant@gmail.com>
2020-09-13 20:33:24 -0500

    finished project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useFileHandlers from './useFileHandlers';
 
-function App() {
+const Input = (props) => (
+  <input 
+    type="file" 
+    name="file-input" 
+    multiple 
+    {...props} 
+  />
+)
+
+const App = () => {
+  const {
+    files,
+    pending,
+    next,
+    uploading,
+    uploaded,
+    status,
+    onSubmit,
+    onChange,
+  } = useFileHandlers()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="form" onSubmit={onSubmit}>
+        {status === 'FILES_UPLOADED' && (
+          <div className="success-container">
+            <div>
+              <h2>Congratulations!</h2>
+              <small>You uploaded your files. Get some rest.</small>
+            </div>
+          </div>
+        )}
+        <div>
+          <Input onChange={onChange} />
+          <button type="submit">Submit</button>
+        </div>
+        <div>
+          {files.map(({ file, src, id }, index) => (
+            <div key={`thumbs${index}`} className="thumbnail-wrapper" >
+              <img className="thumbnail" src={src} alt ="" />
+              <div className="thumbnail-caption">{file.name}</div>
+            </div>
+          ))}
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
